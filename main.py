f42ca17c1723eeb85a2f96964af6dfaa5373bb51
atsushi <oatsu124a@gmail.com>
2026-05-17 15:52:20 +0900

    3の倍数と5の倍数の判定

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from pyst!")
-
-
-if __name__ == "__main__":
-    main()
+num = int(input ("自然数を入力してください: "))
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")   
+elif num % 5 == 0:
+    print("Buzz")
+else:    print(str(num))
