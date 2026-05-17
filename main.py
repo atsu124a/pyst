4b9406e05f44bf992934d9531f887b30fe096cec
atsushi <oatsu124a@gmail.com>
2026-05-17 16:06:52 +0900

    判定

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-num = int(input ("自然数を入力してください: "))
-if num % 3 == 0 and num % 5 == 0:
-    print("FizzBuzz")
-elif num % 3 == 0:
-    print("Fizz")   
-elif num % 5 == 0:
-    print("Buzz")
-else:    print(str(num))
+def fizz_buzz(num):
+
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+    
+
+
+print(fizz_buzz(int(input ("自然数を入力してください: "))))
